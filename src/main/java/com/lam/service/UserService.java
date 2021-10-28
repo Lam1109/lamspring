7e7b646352642a5310ef19830067488df2a1235e
Lam <1031941624@qq.com>
2021-10-28 23:04:57 +0800

    day 1: prepare

---
+++ src/main/java/com/lam/service/UserService.java
@@ -0,0 +1,7 @@
+package com.lam.service;
+
+import com.lam.Component;
+
+@Component
+public class UserService {
+}
---
+++ src/main/java/com/spring/AppConfig.java
@@ -0,0 +1,7 @@
+package com.spring;
+
+import com.lam.ComponentScan;
+
+@ComponentScan("com.lam.service")
+public class AppConfig {
+}
