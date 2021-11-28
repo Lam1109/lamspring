7ea5c20b1417071d083e9a0ab8f3978025b5aac4
Lam <1031941624@qq.com>
2021-11-28 17:23:18 +0800

    day : Autowired

--- src/main/java/com/spring/ApplicationContext.java
+++ src/main/java/com/spring/ApplicationContext.java
@@ -1,21 +1,70 @@
 package com.spring;
 
-import com.lam.Component;
-import com.lam.ComponentScan;
+import com.utils.ConstantUtil;
 
 import java.io.File;
-import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 
 public class ApplicationContext {
+
     private Class configClass;
+    private ConcurrentHashMap<String, Object> singletonObjects = new ConcurrentHashMap<>(); // 单例池
+    private ConcurrentHashMap<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>(); // Key: beanName, Value: beanDefinition
 
     public ApplicationContext(Class configClass) {
         this.configClass = configClass;
-
         // 解析配置类
-        // ComponentScan注解-->扫描路径-->扫描
+        // ComponentScan注解-->扫描路径-->扫描-->BeanDefinition-->BeanDefinitionMap
+        scan(configClass);
 
+        for (Map.Entry<String, BeanDefinition> entry : beanDefinitionMap.entrySet()) {
+            String beanName = entry.getKey();
+            BeanDefinition beanDefinition = entry.getValue();
+            if (beanDefinition.getScope().equals(ConstantUtil.SINGLETON)) {
+                Object bean = createBean(beanName, beanDefinition); // 创建单例Bean对象
+                singletonObjects.put(beanName, bean);
+            }
+
+        }
+    }
+
+    public Object createBean(String beanName, BeanDefinition beanDefinition) {
+        Class clazz = beanDefinition.getClazz();
+        try {
+            Object instance = clazz.getDeclaredConstructor().newInstance();
+
+            // 依赖注入
+            for (Field declaredField : clazz.getDeclaredFields()) {
+                if (declaredField.isAnnotationPresent(Autowired.class)) {
+                    Object bean = getBean(declaredField.getName());
+
+                    declaredField.setAccessible(true); // private属性可访问
+                    declaredField.set(instance, bean);
+                }
+            }
+
+            if (instance instanceof BeanNameAware) {
+                ((BeanNameAware) instance).setBeanName(beanName);
+            }
+            return instance;
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void scan(Class configClass) {
         ComponentScan componentScanAnnotation = (ComponentScan) configClass.getDeclaredAnnotation(ComponentScan.class);
         String path = componentScanAnnotation.value(); // 扫描路径 com.lam.service
         path = path.replace(".", "/"); //"com/lam/service"
@@ -34,7 +83,22 @@ public class ApplicationContext {
                     try {
                         Class<?> clazz = classLoader.loadClass(className);
                         if (clazz.isAnnotationPresent(Component.class)) {
+                            // 表示当前类是一个Bean
+                            // 解析类 -> BeanDefinition，判断当前类是否单例Bean
+                            // BeanDefinition
+                            Component componentAnnotation = clazz.getDeclaredAnnotation(Component.class);
+                            String beanName = componentAnnotation.value();
 
+                            BeanDefinition beanDefinition = new BeanDefinition();
+                            beanDefinition.setClazz(clazz);
+
+                            if (clazz.isAnnotationPresent(Scope.class)) {
+                                Scope scopeAnnotation = clazz.getDeclaredAnnotation(Scope.class);
+                                beanDefinition.setScope(scopeAnnotation.value());
+                            } else {
+                                beanDefinition.setScope(ConstantUtil.SINGLETON);
+                            }
+                            beanDefinitionMap.put(beanName, beanDefinition);
                         }
                     } catch (ClassNotFoundException e) {
                         e.printStackTrace();
@@ -46,6 +110,19 @@ public class ApplicationContext {
     }
 
     public Object getBean(String beanName) {
-        return null;
+        if (beanDefinitionMap.containsKey(beanName)) {
+            BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
+            if (beanDefinition.getScope().equals(ConstantUtil.SINGLETON)) {
+                Object bean = singletonObjects.get(beanName);
+                return bean;
+            } else {
+                // 创建bean对象
+                Object bean = createBean(beanName, beanDefinition);
+                return bean;
+            }
+        } else {
+            // 不存在对应的bean
+            throw new NullPointerException();
+        }
     }
 }
---
+++ src/main/java/com/utils/ConstantUtil.java
@@ -0,0 +1,6 @@
+package com.utils;
+
+public class ConstantUtil {
+    public static final String SINGLETON = "singleton";
+
+}
